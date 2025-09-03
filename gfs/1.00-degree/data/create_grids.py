608a9645d46737978a2bc7e5c9205e531a248602
Timothy Andrew Smith <smith.timothy.andrew@gmail.com>
2025-09-03 15:37:13 -0700

    create latent mesh when creating target data space

--- gfs/1.00-degree/data/create_grids.py
+++ gfs/1.00-degree/data/create_grids.py
@@ -1,14 +1,13 @@
-"""
-Create a 1 degree global grid to coarsen the 1/4 degree data to
-"""
-
 import os
+import numpy as np
 import xesmf
+
+from anemoi.graphs.generate.utils import get_coordinates_ordering
 
 if __name__ == "__main__":
 
 
-    # global
+    # 1.00 degree data space
     store_dir = f"{os.environ['SCRATCH']}/anemoi-house/gfs/1.00-degree/data"
     if not os.path.isdir(store_dir):
         os.makedirs(store_dir)
@@ -20,3 +19,19 @@ if __name__ == "__main__":
     ds = ds.sortby("lat", ascending=False)
 
     ds.to_netcdf(f"{store_dir}/global_one_degree.nc")
+
+    # 2.00 degree latent space
+    store_dir = f"{os.environ['SCRATCH']}/anemoi-house/gfs/1.00-degree/latlon"
+    if not os.path.isdir(store_dir):
+        os.makedirs(store_dir)
+
+    ds = xesmf.util.grid_global(2, 2, cf=True, lon1=360)
+    ds = ds.drop_vars("latitude_longitude")
+    glon, glat = np.meshgrid(ds["lon"], ds["lat"])
+
+    coords = np.stack([glon.flatten(), glat.flatten()], axis=-1)
+    order = get_coordinates_ordering(coords)
+    glon = coords[order, 0]
+    glat = coords[order, 1]
+
+    np.savez(f"{store_dir}/latentx2.global1degree.sorted.npz", lon=glon, lat=glat)
